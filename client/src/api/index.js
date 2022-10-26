5d731a0e06ae38a993fdaa43b0318984500f86fc
rohit <rhtry.tech@gmail.com>
2022-10-27 00:43:51 +0530

    adding files !

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,10 @@
+import axios from "axios";
 
-//For making API calls
-import axios from 'axios'; 
+const url = "http://localhost:5000/posts";
 
-// URL pointing to the backend route
-const url = 'http://localhost:5000/posts'; 
-
-// Exporting fetchPost function to redux folders 
-export const fetchPosts = () => axios.get(url); 
-
-// Function to create new post [/post operation] 
-export const createPost = (newPost) => axios.post(url, newPost); 
+export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+export const updatePost = (id, updatedPost) =>
+  axios.patch(`${url}/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
